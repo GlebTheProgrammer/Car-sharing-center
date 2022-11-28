d3677aa5c7c6bda0aa479c624f64b9f030ab0f7e
Глеб Сукристик <Gleb15a@gmail.com>
2022-11-28 15:28:49 +0300

    Refactoring. Added logout error on Client Profile page

--- CarSharingApp/Controllers/ClientProfileController.cs
+++ CarSharingApp/Controllers/ClientProfileController.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using CarSharingApp.Models.ClientData;
 using CarSharingApp.Repository.Interfaces;
+using CarSharingApp.Services.Includes;
+using CarSharingApp.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarSharingApp.Controllers
@@ -10,16 +12,27 @@ namespace CarSharingApp.Controllers
         private readonly IVehiclesRepository vehiclesRepository;
         private readonly IClientsRepository clientsRepository;
         private readonly IMapper mapper;
+        private readonly ICurrentUserStatusProvider userStatusProvider;
 
-        public ClientProfileController(IVehiclesRepository vehiclesRepository, IClientsRepository clientsRepository, IMapper mapper)
+        public ClientProfileController(IVehiclesRepository vehiclesRepository, IClientsRepository clientsRepository, IMapper mapper, ICurrentUserStatusProvider userStatusProvider)
         {
             this.vehiclesRepository = vehiclesRepository;
             this.clientsRepository = clientsRepository;
             this.mapper = mapper;
+            this.userStatusProvider = userStatusProvider;
         }
 
         public IActionResult Index(int vehicleId)
         {
+            if (userStatusProvider.HasUserLoggedOut())
+                return RedirectToAction("Index", "Home");
+
+            if (userStatusProvider.GetUserRole() != UserRole.Client)
+            {
+                userStatusProvider.ChangeUnauthorizedAccessState(true);
+                return RedirectToAction("Index", "Home");
+            }
+
             var vehicle = vehiclesRepository.GetVehicleById(vehicleId);
 
             var client = clientsRepository.GetClientById(vehicle.OwnerId);
