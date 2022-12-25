7ddd4128a2f00583490c20aefd5ad3fda371bcba
Глеб Сукристик <Gleb15a@gmail.com>
2022-12-25 19:53:01 +0300

    Started implementing errors handling

--- CarSharingApp.PublicApi/Program.cs
+++ CarSharingApp.PublicApi/Program.cs
@@ -5,40 +5,28 @@ using CarSharingApp.Domain.Entities;
 using CarSharingApp.Infrastructure.MongoDB;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddMongo();
-
-//builder.Services.AddMongoRepository<Administrator>(MongoDbConstants.ADMINS_COLLECTION_NAME);
-//builder.Services.AddMongoRepository<Customer>(MongoDbConstants.CUSTOMERS_COLLECTION_NAME);
-//builder.Services.AddMongoRepository<CustomerCredentials>(MongoDbConstants.CUSTOMERS_CREDENTIALS_COLLECTION_NAME);
-//builder.Services.AddMongoRepository<Payment>(MongoDbConstants.PAYMENTS_COLLECTION_NAME);
-//builder.Services.AddMongoRepository<Rental>(MongoDbConstants.RENTALS_COLLECTION_NAME);
-//builder.Services.AddMongoRepository<Review>(MongoDbConstants.REVIEWS_COLLECTION_NAME);
-builder.Services.AddMongoRepository<Vehicle>(MongoDbConstants.VEHICLES_COLLECTION_NAME);
-builder.Services.AddSingleton<IVehicleService, VehicleService>();
-
-//builder.Services.AddSingleton<IVehicleService, VehicleService>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddMongo();
+    builder.Services.AddMongoRepository<Vehicle>(MongoDbConstants.VEHICLES_COLLECTION_NAME);
+    builder.Services.AddSingleton<IVehicleService, VehicleService>();
 }
 
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
+var app = builder.Build();
+{
+    app.UseExceptionHandler("/error");
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
+}
 
-app.MapControllers();
-
-app.Run();
