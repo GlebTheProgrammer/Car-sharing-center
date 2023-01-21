7b63416a93c11317113019ee848a7c8bd7b4e286
Глеб Сукристик <Gleb15a@gmail.com>
2023-01-21 16:37:50 +0300

    Replaced identity server with mvc app and finished making html for customer account page

---
+++ CarSharingApp.IdentityServer/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using CarSharingApp.IdentityServer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarSharingApp.IdentityServer.Controllers
+{
+    [Route("[controller]")]
+    public class AuthController : Controller
+    {
+        [Route("[action]")]
+        public IActionResult SignIn(string returnUrl)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult SignIn(LoginViewModel model)
+        {
+            return View();
+        }
+    }
+}
--- CarSharingApp.Web/Controllers/AccountDashboardController.cs
+++ CarSharingApp.Web/Controllers/AccountDashboardController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CarSharingApp.Web.Controllers
 {
     public class AccountDashboardController : Controller
     {
+        [Authorize]
         public IActionResult Index()
         {
             return View();
